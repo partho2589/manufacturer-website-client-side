e43d832032dc0db45fc7726231055e49621506f8
partho2589 <parthomalo615@.com>
2022-05-22 11:34:06 +0600

    Install tailwind

---
+++ src/Pages/Home/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1 className='text-4xl text-red-800'>This is navbar</h1>
+        </div>
+    );
+};
+
+export default Navbar;
