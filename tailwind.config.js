85071bdc480691b0fb5030ba5573ec7b9754a5df
partho2589 <parthomalo615@.com>
2022-05-22 11:53:25 +0600

    added navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,18 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+   themes: [
+      {
+        mytheme: {
+          primary: "#red",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  plugins: [require("daisyui")],
 }
