b6b668277af2c9b298f2be70ebaeb067c3cbb97d
partho2589 <parthomalo615@.com>
2022-05-22 10:41:50 +0600

    added Pages and home page

---
+++ src/Pages/Shared/CommonButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CommonButton = () => {
+    return (
+        <div>
+            <button></button>
+        </div>
+    );
+};
+
+export default CommonButton;
---
+++ src/Pages/Shared/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div>
+            <h1>This is a loading</h1>
+        </div>
+    );
+};
+
+export default Loading;
