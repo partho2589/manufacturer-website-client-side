ad06db8dca3353d79bc53b9a7f6674872be3f757
partho2589 <parthomalo615@.com>
2022-05-23 17:44:25 +0600

    firebase init and .env.local file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
 import Blog from './Pages/Blog';
 import Home from './Pages/Home/Home';
 import Navbar from './Pages/Home/Navbar';
+import Login from './Pages/Login/Login';
 import Purchase from './Pages/Purchase/Purchase';
 import NotFound from './Pages/Shared/NotFound';
 
 function App() {
   return (
-    <div>
+    <div className='bg-red bg-pink-100'>
       <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/purchase' element={<Purchase></Purchase>}></Route>
         <Route path='/blog' element={<Blog></Blog>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
