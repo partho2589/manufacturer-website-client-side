dc1f5abc44584016136338a14f92ffa42b89754b
partho2589 <parthomalo615@.com>
2022-05-23 17:13:53 +0600

    added Tools all products

--- src/Pages/Home/Product.js
+++ src/Pages/Home/Product.js
@@ -1,17 +1,19 @@
 import React from 'react'
-import img from '../../images/banner.png'
 const Product = ({ product }) => {
-    console.log(product)
+    const {name,img, price,availableQuantity,minimumOrderQuantity,description} = product;
     return (
-        <div class="card lg:max-w-lg bg-base-100 shadow-xl">
-            <figure class="px-10 pt-10">
-                <img src={img} alt="Shoes" class="rounded-xl" />
+        <div className="card lg:max-w-lg bg-base-100 shadow-xl">
+            <figure className="px-10 pt-10">
+                <img src={img} alt="Shoes" className="rounded-xl" />
             </figure>
-            <div class="card-body items-center text-center">
-                <h2 class="card-title">{product.name}</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions">
-                    <button class="btn btn-primary">Buy Now</button>
+            <div className="card-body items-start text-left">
+                <h2 className="card-title text-xl">{name}</h2>
+                <p><span>Price: ${price}</span></p>
+                <p><span>Available Quantity: {availableQuantity}</span></p>
+                <p><span>Minimum Order Quantity: {minimumOrderQuantity}</span></p>
+                <p>{description}</p>
+                <div className="card-actions">
+                    <button className="btn btn-primary">Buy Now</button>
                 </div>
             </div>
         </div>
